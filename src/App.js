e087cb6adcf0ae40b9e7591dadb283e7665cc144
jonnathandeb <jonnathandeb.dev@gmail.com>
2019-12-27 10:42:37 -0600

    Added Routes - need to add imports to pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
 import React from 'react';
 import { Switch, Route, Redirect } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route 
+          exact 
+          path="/" 
+          render={matchProps => (
+            <HomePage {...matchProps} />
+          )} 
+        />
+        <Route  
+          path="/profile/:id" 
+          render={matchProps => (
+            <OtherProfilePage {...matchProps} />
+          )} 
+        />
+        <Route  
+          path="/profile" 
+          render={matchProps => (
+            <SelfProfilePage {...matchProps} />
+          )} 
+        />
+        <Route  
+          path="/feed" 
+          render={matchProps => (
+            <FeedPage {...matchProps} />
+          )} 
+        />
+        <Route  
+          path="/feed/:id" 
+          render={matchProps => (
+            <FeedClassPage {...matchProps} />
+          )} 
+        />
+        <Route  
+          path="/post/:id" 
+          render={matchProps => (
+            <FeedAllPage {...matchProps} />
+          )} 
+        />
+        <Route  
+          path="/post" 
+          render={matchProps => (
+            <MakePostPage {...matchProps} />
+          )} 
+        />
+        <Route  
+          path="/login" 
+          render={matchProps => (
+            <LoginPage {...matchProps} />
+          )} 
+        />
+        <Route  
+          path="/register" 
+          render={matchProps => (
+            <RegisterPage {...matchProps} />
+          )} 
+        />
+        <Route  
+          path="/leaderboard" 
+          render={matchProps => (
+            <LeaderboardPage {...matchProps} />
+          )} 
+        />
+        <Route  
+          path="/search" 
+          render={matchProps => (
+            <Search {...matchProps} />
+          )} 
+        />
+      </Switch>
+      <Footer/>
     </div>
   );
 }
