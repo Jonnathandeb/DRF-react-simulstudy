355130f46da3391d0e7465eee9e22515f414aa26
jonnathandeb <jonnathandeb.dev@gmail.com>
2020-03-16 17:03:53 -0500

    Added dummy tabs on leaderboard page for school and classes

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import { Menu, Grid, Segment } from 'semantic-ui-react'
 
 export class LeaderboardPage extends Component {
-    render() {
-        return (
-            <div>Leaderboard Page</div>
-        );
-    }
+	state = { activeItem: 'home' }
+
+	handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+	render() {
+		const { activeItem } = this.state
+
+		return (
+			<Grid>
+				<Grid.Column width={3}>
+				<Menu pointing secondary vertical>
+					<Menu.Item
+						name='home'
+						active={activeItem === ''}
+						onClick={this.handleItemClick}
+					/>
+					<Menu.Item
+						name='messages'
+						active={activeItem === 'messages'}
+						onClick={this.handleItemClick}
+					/>
+					<Menu.Item
+						name='friends'
+						active={activeItem === 'friends'}
+						onClick={this.handleItemClick}
+					/>
+				</Menu>
+				</Grid.Column>
+
+				<Grid.Column width={13}>
+					
+				</Grid.Column>
+			</Grid>
+		)
+	}
 }
