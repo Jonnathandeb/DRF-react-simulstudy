5bc47b41b404107f885476de0adbf369e2b89ee8
jonnathandeb <jonnathandeb.dev@gmail.com>
2020-03-16 08:51:25 -0500

    Added uploading images to AWS after submitting post, need to get it working later

--- src/pages/MakePost.js
+++ src/pages/MakePost.js
@@ -8,97 +8,141 @@ import { Redirect } from 'react-router-dom';
 import config from "../api_config.json";
 
 export class MakePostPage extends Component {
-    state = { class: '', title: '', content: '', file: ''}
-    fileInputRef = React.createRef();
+	state = { class: '', title: '', content: '', file: ''}
+	fileInputRef = React.createRef();
 
-    handleChange = (e, { name, value }) => this.setState({ [name]: value })
+	handleChange = (e, { name, value }) => this.setState({ [name]: value })
 
-    fileChange = e => {
-        this.setState({ file: e.target.files[0] }, () => {
-            console.log("File chosen --->", this.state.file);
-        });
-    };
+	fileChange = e => {
+		this.setState({ file: e.target.files[0] }, () => {
+			console.log("File chosen --->", this.state.file);
+		});
+	};
 
-    fileUpload = file => {
-        const url = "/some/path/to/post";
-        const formData = new FormData();
-        formData.append("file", file);
-        const config = {
-            headers: {
-            "Content-type": "multipart/form-data"
-            }
-        };
-        //return put(url, formData, config);
-    };
+	fileUpload = file => {
+		this.setState({file: file});
+	};
 
-    handleSubmit = () => {
-        let postData = {"post_class": this.state.class, "title": this.state.title, "content": this.state.content, "image_url": this.state.link};
+	getPresignedURL = (file) => {
+		let fileType = file.type;
+		let fileName = file.name;
 
-        fetch(`${config.url}/posts/`,{
-            headers: new Headers({
-                'Authorization': 'Bearer  ' + getSession().jwt, 
-                'Content-Type': 'application/json',
-            }), 
-            method: 'post',
-            body: JSON.stringify(postData),
-        })
-        .then(res => res.json())
-        .then((res) => {
-            if (res.url) {
-                let redUrl = res.url.substring(res.url.indexOf(`${config.url}/posts/`) + 21);
-                this.setState({redirect: "post/" + redUrl});
-            }
+		let postData = {"file_name": fileName, "file_type": fileType}
+
+		fetch(`${config.url}/get_presigned_url/`,{
+			headers: new Headers({
+				'Authorization': 'Bearer  ' + getSession().jwt, 
+				'Content-type': 'application/json'
+			}), 
+			method: 'post',
+			body: JSON.stringify(postData),
 		})
-    }
-    
-    render() {
-        if (this.state.redirect) 
-            return <Redirect to={this.state.redirect} />
+		.then(res => res.json())
+		.then((res) => {
+			if (res.Success) {
+				let signedURL = res.response.url;
+				let signedData = res.response.fields;
+				let formData = new FormData();
 
-        return (
-            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-                <Grid.Column style={{ maxWidth: 450 }}>
-                <Header as='h2' textAlign='center'>
-                    <Image src='/SS_logo.png' /> Create Post
-                </Header>
-                <Form size='large' onSubmit={this.handleSubmit}>
-                    <Segment stacked>
-                    <ClassesSelect id="5" handleChange={this.handleChange} />
-                    <Form.Input 
-                        fluid 
-                        placeholder='Title'
-                        name="title"
-                        onChange={this.handleChange}
-                    />
-                    <Form.TextArea
-                        fluid
-                        placeholder='Content'
-                        name="content"
-                        onChange={this.handleChange}
-                    />
-                    <Button
-                        fluid
-                        size="medium"
-                        content="Choose File (Optional)"
-                        labelPosition="left"
-                        icon="file"
-                        onClick={() => this.fileInputRef.current.click()}
-                        type="button"
-                    />
-                    <input
-                        ref={this.fileInputRef}
-                        type="file"
-                        hidden
-                        onChange={this.fileChange}
-                    />
-                    <br/>
-                    <Button fluid size='large' type="submit">
-                        Post
-                    </Button>
-                    </Segment>
-                </Form>
-                </Grid.Column>
-		    </Grid>
-        );
-    }
+				for (let key in signedData) {
+					formData.append(key, formData[key]);
+				}
+
+				formData.append("file", file, formData["key"])
+
+				console.log(signedData)
+				fetch(`${signedURL}?X-Amz-Algorithm=${signedData["x-amz-algorithm"]}&X-Amz-Credential=${signedData["x-amz-credential"]}&X-Amz-Date=${signedData["x-amz-date"]}&X-Amz-Expires=${signedData["Expires"]}&X-Amz-SignedHeaders=&X-Amz-Signature=${signedData["x-amz-signature"]}`,{
+					headers: new Headers({
+						"enctype": "multipart/form-data",
+					}), 
+					method: 'POST',
+					body: formData
+				})
+				.then(res => res.text())
+				.then((res) => {
+					console.log(res)
+					console.log(`${signedURL}?X-Amz-Algorithm=${signedData["x-amz-algorithm"]}&X-Amz-Credential=${signedData["x-amz-credential"]}&X-Amz-Date=${signedData["x-amz-date"]}&X-Amz-Expires=${signedData["Expires"]}&X-Amz-SignedHeaders=host&X-Amz-Signature=${signedData["x-amz-signature"]}`)
+				}).catch((err) => {
+					console.log(err)
+				})
+			}
+			else {
+				console.log("failed")
+			}
+		})
+	}
+
+	handleSubmit = () => {
+		let postData = {"post_class": this.state.class, "title": this.state.title, "content": this.state.content, "image_url": this.state.file};
+
+		this.getPresignedURL(this.state.file)
+
+		/*fetch(`${config.url}/posts/`,{
+			headers: new Headers({
+				'Authorization': 'Bearer  ' + getSession().jwt, 
+				'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW',
+			}), 
+			method: 'post',
+			body: JSON.stringify(postData),
+		})
+		.then(res => res.json())
+		.then((res) => {
+			console.log(res)    
+			if (res.url) {
+				let redUrl = res.url.substring(res.url.indexOf(`${config.url}/posts/`) + 21);
+				this.setState({redirect: "post/" + redUrl});
+			}
+		})*/
+	}
+	
+	render() {
+		if (this.state.redirect) 
+			return <Redirect to={this.state.redirect} />
+
+		return (
+			<Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+				<Grid.Column style={{ maxWidth: 450 }}>
+				<Header as='h2' textAlign='center'>
+					<Image src='/SS_logo.png' /> Create Post
+				</Header>
+				<Form size='large' onSubmit={this.handleSubmit}>
+					<Segment stacked>
+					<ClassesSelect id="5" handleChange={this.handleChange} />
+					<Form.Input 
+						fluid 
+						placeholder='Title'
+						name="title"
+						onChange={this.handleChange}
+					/>
+					<Form.TextArea
+						fluid
+						placeholder='Content'
+						name="content"
+						onChange={this.handleChange}
+					/>
+					<Button
+						fluid
+						size="medium"
+						content="Choose File (Optional)"
+						labelPosition="left"
+						icon="file"
+						onClick={() => this.fileInputRef.current.click()}
+						type="button"
+					/>
+					<input
+						ref={this.fileInputRef}
+						type="file"
+						hidden
+						onChange={this.fileChange}
+					/>
+					<br/>
+					<Button fluid size='large' type="submit">
+						Post
+					</Button>
+					</Segment>
+				</Form>
+				</Grid.Column>
+			</Grid>
+		);
+	}
 }
